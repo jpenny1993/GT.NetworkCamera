f29f5a3c0a0645a4f0ae1a8ddf52866ee1339f5e
John Penny <jpenny1993@users.noreply.github.com>
2022-07-20 16:39:28 +0100

    Update time synchronisation

---
+++ src/MyHome/Constants/DateTypes.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.SPOT;
+
+namespace MyHome.Constants
+{
+    public static class DateTypes
+    {
+        public const string NIST = "nist";
+        public const string NTP = "ntp";
+    }
+}
--- src/MyHome/Modules/ISystemManager.cs
+++ src/MyHome/Modules/ISystemManager.cs
@@ -4,7 +4,6 @@ namespace MyHome.Modules
 {
     public interface ISystemManager
     {
-        bool IsTimeSynchronised { get; }
         DateTime StartTime { get; }
         DateTime Time { get; }
         TimeSpan Uptime { get; }
